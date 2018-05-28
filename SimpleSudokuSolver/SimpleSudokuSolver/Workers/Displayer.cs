b04669fdf042501f16cedc8d42b44628a2f5dbfb
Masza <Masza@DESKTOP-N7AA2GL>
2018-05-28 07:27:01 +0200

    New classess added

---
+++ SimpleSudokuSolver/SimpleSudokuSolver/Workers/Displayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSudokuSolver.Workers
+{
+    class Displayer
+    {
+        public void Display(int[,] sudokuBoard)
+        {
+
+        }
+
+    }
+}
