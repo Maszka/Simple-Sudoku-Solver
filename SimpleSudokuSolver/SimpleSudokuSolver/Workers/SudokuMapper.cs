62997cc848436cea8ca021548edc100ca1194104
Masza <Masza@DESKTOP-N7AA2GL>
2018-05-28 17:21:54 +0200

    SudokuMapper implementation

--- SimpleSudokuSolver/SimpleSudokuSolver/Workers/SudokuMapper.cs
+++ SimpleSudokuSolver/SimpleSudokuSolver/Workers/SudokuMapper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleSudokuSolver.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,66 @@ namespace SimpleSudokuSolver.Workers
 {
     class SudokuMapper
     {
+        public SudokuMap FindStart(int row, int col)
+        {
+            SudokuMap map = new SudokuMap();
+
+            if ((row >= 0 && row < 3) && (col>=0 && col<3))
+            {
+                map.StartRow = 0;
+                map.StartCol = 0;
+            }
+
+            if ((row >= 0 && row < 3) && (col >= 3 && col < 6))
+            {
+                map.StartRow = 0;
+                map.StartCol = 3;
+            }
+
+            if ((row >= 0 && row < 3) && (col >= 6 && col < 9))
+            {
+                map.StartRow = 0;
+                map.StartCol = 6;
+            }
+
+            if ((row >= 3 && row < 6) && (col >= 0 && col < 3))
+            {
+                map.StartRow = 3;
+                map.StartCol = 0;
+            }
+
+            if ((row >= 3 && row < 6) && (col >= 3 && col < 6))
+            {
+                map.StartRow = 3;
+                map.StartCol = 3;
+            }
+
+            if ((row >= 3 && row < 6) && (col >= 6 && col < 9))
+            {
+                map.StartRow = 3;
+                map.StartCol = 6;
+            }
+
+            if ((row >= 6 && row < 9) && (col >= 0 && col < 3))
+            {
+                map.StartRow = 6;
+                map.StartCol = 0;
+            }
+
+            if ((row >= 6 && row < 9) && (col >= 3 && col < 6))
+            {
+                map.StartRow = 6;
+                map.StartCol = 3;
+            }
+
+            if ((row >= 6 && row < 9) && (col >= 6 && col < 9))
+            {
+                map.StartRow = 6;
+                map.StartCol = 6;
+            }
+
+            return map;
+        }
+
     }
 }
