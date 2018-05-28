f127549d2f19b5d5604d5fb7afa5f789dcae92aa
Masza <Masza@DESKTOP-N7AA2GL>
2018-05-28 18:13:18 +0200

    SudokuEngine implementation

---
+++ SimpleSudokuSolver/SimpleSudokuSolver/Strategies/SimpleMarkUpStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSudokuSolver.Strategies
+{
+    class SimpleMarkUpStrategy : IStrategy
+    {
+        public int[,] Solve(int[,] sudokuBoard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SimpleSudokuSolver/SimpleSudokuSolver/Strategies/SudokuEngine.cs
+++ SimpleSudokuSolver/SimpleSudokuSolver/Strategies/SudokuEngine.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleSudokuSolver.Workers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,36 @@ namespace SimpleSudokuSolver.Strategies
 {
     class SudokuEngine
     {
+        private readonly SudokuBoardStateManager _sudokuBoardStateManager;
+        private readonly SudokuMapper _sudokuMapper;
+
+        public SudokuEngine(SudokuBoardStateManager sudokuBoardStateManager, SudokuMapper sudokuMapper)
+        {
+            _sudokuBoardStateManager = sudokuBoardStateManager;
+            _sudokuMapper = sudokuMapper;
+        }
+
+        public bool SolveSudoku(int[,] sudokuBoard)
+        {
+            List<IStrategy> strategies = new List<IStrategy>()
+            {
+
+            };
+
+            var currentState = _sudokuBoardStateManager.GenerateState(sudokuBoard);
+            var nextState = _sudokuBoardStateManager.GenerateState(strategies.First().Solve(sudokuBoard));
+
+            if (!_sudokuBoardStateManager.IsSolved(sudokuBoard))
+            {
+                nextState = currentState;
+
+                foreach (var strategy in strategies)
+                {
+                    nextState = _sudokuBoardStateManager.GenerateState(strategy.Solve(sudokuBoard));
+                }
+            }
+
+            return _sudokuBoardStateManager.IsSolved(sudokuBoard);
+        }
     }
 }
