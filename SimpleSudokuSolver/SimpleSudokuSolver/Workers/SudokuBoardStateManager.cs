c7b3c2e9708335d9214d2f6d9e9164b83b12f2cd
Masza <Masza@DESKTOP-N7AA2GL>
2018-05-28 17:46:48 +0200

    SudokuBoardStateManager implementation

--- SimpleSudokuSolver/SimpleSudokuSolver/Workers/SudokuBoardStateManager.cs
+++ SimpleSudokuSolver/SimpleSudokuSolver/Workers/SudokuBoardStateManager.cs
@@ -8,5 +8,31 @@ namespace SimpleSudokuSolver.Workers
 {
     class SudokuBoardStateManager
     {
+        public string GenerateState(int[,] sudokuBoard)
+        {
+            StringBuilder key = new StringBuilder();
+
+            for(int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    key.Append(sudokuBoard[row, col]);
+                }
+            }
+
+            return key.ToString();
+        }
+
+        public bool IsSolved(int[,] sudokuBoard)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    if (sudokuBoard[row, col] == 0 || sudokuBoard[row, col].ToString().Length > 1) return false;
+                }
+            }
+            return true;
+        }
     }
 }
