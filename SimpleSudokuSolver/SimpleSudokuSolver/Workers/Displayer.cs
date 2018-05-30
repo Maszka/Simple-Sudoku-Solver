29fef30c30428800e525a2f39c83071779901bb5
Masza <Masza@DESKTOP-N7AA2GL>
2018-05-30 08:27:34 +0200

    Displayer implementation

--- SimpleSudokuSolver/SimpleSudokuSolver/Workers/Displayer.cs
+++ SimpleSudokuSolver/SimpleSudokuSolver/Workers/Displayer.cs
@@ -8,9 +8,21 @@ namespace SimpleSudokuSolver.Workers
 {
     class Displayer
     {
-        public void Display(int[,] sudokuBoard)
+        public void Display(string title, int[,] sudokuBoard)
         {
+            Console.WriteLine(title + ":");
 
+            for (int row = 0; row < 9; row++)
+            {
+                Console.WriteLine("|");
+
+                for (int col = 0; col < 9; col++)
+                {
+                    Console.Write(sudokuBoard[row,col] + "|");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
 
     }
